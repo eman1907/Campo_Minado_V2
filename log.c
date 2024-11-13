1471c2388b74eb7cd0fd106b60e0bcf30e1d3292
eman1907 <emanuel.moraes@icomp.ufam.edu.br>
2024-11-13 12:32:18 -0300

    Create log.c

---
+++ log.c
@@ -0,0 +1,38 @@
+#include <stdlib.h> 
+#include <time.h> 
+#include "log.h" 
+
+Log* abrir_log(){ 
+	Log* log = malloc(sizeof(Log)); 
+	log->arquivo = fopen("log.txt" , "w"); 
+	if (log->arquivo == NULL){ 
+		printf("Erro ao escrever em arquivo\n"); 
+		return NULL; 
+	} 
+	time_t t = time(NULL); 
+	struct tm tm = *localtime(&t); 
+	fprintf(log->arquivo, "%02d/%02d/%04d %02d:%02d:%02d\n", tm.tm_mday, tm.tm_mon + 1, 
+		tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec); 
+	return log; 
+}
+
+ void registrar_jogada(Log* log, int** mat, int n, int x, int y){ 
+ 	for (int i = 0; i < n; i++){ 
+ 		for (int j = 0; j < n; j++){ 
+ 			if(mat[i][j] == -10) fprintf(log->arquivo," x"); 
+ 			else if(mat[i][j] == -1 && (x != i || y != j)) fprintf(log->arquivo," x");
+ 			 else if(mat[i][j] == -1 && (x == i && y == j)) fprintf(log->arquivo," %d" , mat[i][j]); 
+ 			 else fprintf(log->arquivo," %d" , mat[i][j]); 
+ 			  } 
+ 			fprintf(log->arquivo, "\n"); 
+ 		  } 
+ 		fprintf(log->arquivo, "\n");
+ 	 } 
+
+
+ void fechar_log(Log* log){
+ 	if(log != NULL && log->arquivo != NULL){ 
+ 		fclose(log->arquivo); 
+ 		} 
+ 	free(log); 
+ 	}
