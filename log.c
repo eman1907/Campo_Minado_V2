3473047f6d8f448ff7e6b546890ccb86550e5137
eman1907 <emanuel.moraes@icomp.ufam.edu.br>
2024-11-13 18:07:43 -0400

    Update log.c

--- log.c
+++ log.c
@@ -13,16 +13,18 @@ Log* abrir_log(){
 	struct tm tm = *localtime(&t); 
 	fprintf(log->arquivo, "%02d/%02d/%04d %02d:%02d:%02d\n", tm.tm_mday, tm.tm_mon + 1, 
 		tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec); 
+	fprintf(log->arquivo, "\n");
 	return log; 
 }
 
  void registrar_jogada(Log* log, int** mat, int n, int x, int y){ 
+	fprintf(log->arquivo,"Coordenada jogada: %d, %d\n" , x + 1, y + 1);
  	for (int i = 0; i < n; i++){ 
  		for (int j = 0; j < n; j++){ 
- 			if(mat[i][j] == -10) fprintf(log->arquivo," x"); 
- 			else if(mat[i][j] == -1 && (x != i || y != j)) fprintf(log->arquivo," x");
+ 			if(mat[i][j] == -10) fprintf(log->arquivo,"  x"); 
+ 			else if(mat[i][j] == -1 && (x != i || y != j)) fprintf(log->arquivo,"  x");
  			 else if(mat[i][j] == -1 && (x == i && y == j)) fprintf(log->arquivo," %d" , mat[i][j]); 
- 			 else fprintf(log->arquivo," %d" , mat[i][j]); 
+ 			 else fprintf(log->arquivo,"  %d" , mat[i][j]); 
  			  } 
  			fprintf(log->arquivo, "\n"); 
  		  } 
