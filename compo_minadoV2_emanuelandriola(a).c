027323c1cf9c6a4fa29e866ace9dc9f5ca621985
eman1907 <emanuel.moraes@icomp.ufam.edu.br>
2024-11-14 17:48:47 -0400

    Update compo_minadoV2_emanuelandriola(a).c

--- compo_minadoV2_emanuelandriola(a).c
+++ compo_minadoV2_emanuelandriola(a).c
@@ -1,8 +1,8 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <time.h> 
-#include "matriz.h" 
-#include "log.h"
+#include "matriz.c" 
+#include "log.c"
 
 int main(){ 
 int dif, n, n2, n3, n4; 
@@ -12,8 +12,8 @@ printf("Digite o número da dificuldade que deseja jogar:\n");
 printf("1 - Fácil\n2 - Médio\n3 - Difícil\n"); 
 
 scanf("%d" , &dif); 
+//lê a dificuldade do jogo conforme especificado no enunciado;
 variaveis_dificuldade(dif, &n, &n2, &n3, &n4);
-//lê a dificuldade do jogo conforme especificado no enunciado variaveis_dificuldade(dif, &n, &n2, &n3, &n4);
 
 if(dif == 1 || dif == 2 || dif == 3){ //só permite a entrada em caso de numeros validos 
 
@@ -25,7 +25,8 @@ int acum = 0; //contará quantas bombas tem próximo a cada coordenada, exceto -
 int cont = 0; //irá de 0 a n3 para indicar se o usuario ganhou o jogo 
 
 printf("Coloque as coordenadas desejadas no estilo x,y "); 
-printf("e repita o processo ao final de cada rodada.\n"); printf("obs: não se esqueça da vírgula.\n"); 
+printf("e repita o processo ao final de cada rodada.\n"); 
+printf("obs: não se esqueça da vírgula.\n"); 
 
 //imprime a matriz inicial para o usuário ter noção das dimensões 
 for (int i = 0; i < n; i++){ 
@@ -42,10 +43,12 @@ while (scanf("%d,%d" , &x, &y)){
 
 	  if ((x < 1 || y < 1) || (x > n || y > n)){ 
 	 printf("Coordenadas indisponíveis, tente outros números.\n"); 
+	 coordenada_errada(log, x, y, 1); //funcao de log.c
 	   }
 
 	  else if (mat2[x - 1][y - 1] != - 10 && mat2[x - 1][y - 1] != -1){  
 	 printf("Coordenadas repetidas, tente outros números.\n"); 
+	 coordenada_errada(log, x, y, 0); //funcao de log.c
 	   } 
 
 	  else {
@@ -54,7 +57,7 @@ while (scanf("%d,%d" , &x, &y)){
 
 	    if (mat[x][y] == -10) floodfill(mat2, x, y, n4, &cont); 
 		
-	   registrar_jogada(log, mat2, n, x, y); 
+	   registrar_jogada(log, mat2, n, x, y); //funcao de log.c
 
 	  for (int i = 0; i < n; i++){ 
 		for (int j = 0; j < n; j++){ 
@@ -78,10 +81,10 @@ while (scanf("%d,%d" , &x, &y)){
 	}
 
 if (cont == n3) ok = 1; 
-	else ok = 0;
+else ok = 0;
 
-campo_final(log, mat2, n, ok);
-fechar_log(log);
+campo_final(log, mat2, n, ok); //funcao de log.c
+fechar_log(log); //funcao de log.c
 
 
 //imprime a mensagem em caso de vitória 
@@ -103,7 +106,8 @@ for (int i = 0; i < n; i++){
 	printf("\n"); 
 } 
 
-liberar_matriz(mat3, n); 
+
+liberar_matriz(mat, n); 
 } 
 
 return 0; 
