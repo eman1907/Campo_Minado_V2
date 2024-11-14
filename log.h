72ccc4b9d233241c455870d5ab99c6f8cb7c5fa9
eman1907 <emanuel.moraes@icomp.ufam.edu.br>
2024-11-14 17:49:43 -0400

    Update log.h

--- log.h
+++ log.h
@@ -6,15 +6,23 @@ typedef struct {
 	FILE* arquivo; 
 } Log; 
 
-//abre o arquivo dentro da função leitura_das_coordenadas e imprime data e hora que o usuário iniciou o jogo
+//abre o arquivo e imprime data e hora que o usuário iniciou o jogo
 Log* abrir_log(); 
 
-//registra cada jogada do usuário imprimindo o campo dentro da matriz
+//imprime quando a coordenada não existe ou já foi iniciaizada, 
+//'det' funciona como diferenciação entre repetida(passa 0 como parametro),
+//e inexistente(passa 1 como parametro)
+void coordenada_errada(Log* log, int x, int y, int det);
+
+//registra cada jogada do usuário imprimindo o campo dentro do arquivo log.txt incluindo
+//as escolhas de jogada do usuário
 void registrar_jogada(Log* log, int** mat, int n, int x, int y); 
 
-//imprime a mensagem que informa o usuário em caso de vitória ou derrota
-//e imprime a matriz completa sem a presença do 'x' ao final do jogo
+/*imprime a mensagem que informa o usuário em caso de vitória ou derrota
+e imprime a matriz completa sem a presença do 'x' ao final do jogo,
+recebe dentro da função o 'inicializa_coordenadas' para calcular o valor
+de cada coordenada*/
 void campo_final(Log* log, int** mat, int n, int ok);
 
-//fecha o arquivo ao fim da função leitura_das_coordenadas
+//fecha o arquivo 
 void fechar_log(Log* log);
