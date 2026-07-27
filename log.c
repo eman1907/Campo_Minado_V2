552a82cd651bc951a58b117cfd63fbb107ab1421
eman1907 <emanuel.moraes@icomp.ufam.edu.br>
2026-07-27 13:27:12 -0300

    Update log.c

--- log.c
+++ log.c
@@ -1,65 +1,48 @@
-#include <stdlib.h> 
-#include <time.h> 
-#include "log.h" 
-#include "matriz.h" 
+#include "log.h"
 
-Log* abrir_log(){ 
-	Log* log = malloc(sizeof(Log)); 
-	log->arquivo = fopen("log.txt" , "w"); 
-	if (log->arquivo == NULL){ 
-		printf("Erro ao escrever em arquivo\n"); 
-		return NULL; 
-	} 
-	time_t t = time(NULL); 
-	struct tm tm = *localtime(&t); 
-	fprintf(log->arquivo, "%02d/%02d/%04d %02d:%02d:%02d\n", tm.tm_mday, tm.tm_mon + 1, 
-		tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec); 
-	fprintf(log->arquivo, "\n");
-	return log; 
+FILE* criar_arquivo(){
+    FILE* arq = fopen("log.txt" , "w");
+    if (arq == NULL){
+        printf("Memoria insuficiente\n");
+        exit(1);
+    }
+    return arq; 
 }
 
-void coordenada_errada(Log* log, int x, int y, int det){ 
-	if(det) fprintf(log->arquivo, "Coordenadas %d, %d inexistentes, tente outros números.\n\n" , x, y); 
-	else fprintf(log->arquivo, "Coordenadas %d, %d repetidas, tente outros números.\n\n" , x, y);
+void imprime_arquivo_log(FILE* arq, int** campo, int tam, int x, int y){
+    fprintf(arq, "Coordenadas: %d, %d\n" , x, y);
+    for (int i = 1; i < tam - 1; i++){
+        for (int j = 1; j < tam - 1; j++){
+            if (campo[i][j] != 10 && campo[i][j] != MINA) fprintf(arq, "  %d" , campo[i][j]);
+            else fprintf(arq, "  x");
+            }
+        fprintf(arq, "\n");
+    }
+    fprintf(arq, "\n"); 
 }
 
- void registrar_jogada(Log* log, int** mat, int n, int x, int y){ 
-	fprintf(log->arquivo,"Coordenada jogada: %d, %d\n" , x + 1, y + 1);
- 	for (int i = 0; i < n; i++){ 
- 		for (int j = 0; j < n; j++){ 
- 			if(mat[i][j] == -10) fprintf(log->arquivo,"  x"); 
- 			else if(mat[i][j] == -1 && (x != i || y != j)) fprintf(log->arquivo,"  x");
- 			 else if(mat[i][j] == -1 && (x == i && y == j)) fprintf(log->arquivo," %d" , mat[i][j]); 
- 			 else fprintf(log->arquivo,"  %d" , mat[i][j]); 
- 			  } 
- 			fprintf(log->arquivo, "\n"); 
- 		  } 
- 		fprintf(log->arquivo, "\n");
- 	 } 
-
-void campo_final(Log* log, int** mat, int n, int ok){
-	for (int x = 0; x < n; x++){
-		for (int y = 0; y < n; y++){
-			if (mat[x][y] != -1) mat[x][y] = inicializa_coordenadas(mat, x, y, n - 1);
-		}
-	}
-
-	if(ok) fprintf(log->arquivo, "Parabéns, vc eh fera!\n");
-	else fprintf(log->arquivo, "Game over!\n");
-
-	for (int x = 0; x < n; x++){
-		for (int y = 0; y < n; y++){
-		if(mat[x][y] == -1)	fprintf(log->arquivo, " %d" , mat[x][y]);
-		else fprintf(log->arquivo, "  %d" , mat[x][y]);
-		}
-	fprintf(log->arquivo, "\n");
-	}
+void imprimeCampo_arq(FILE* arq, int** campo, int tam){
+    for (int i = 1; i < tam - 1; i++){
+        for (int j = 1; j < tam - 1; j++){
+            if (campo[i][j] == MINA) fprintf(arq, " %d" , MINA);
+            else fprintf(arq, "  %d" , campo[i][j]);
+        }
+        fprintf(arq, "\n"); 
+    }
 }
 
+void arq_fimDeJogo(FILE* arq, int det){
+    if (det){
+        fprintf(arq, "Game Over, vc perdeu!\n");
+    } else {
+        fprintf(arq, "Parabens, voce venceu!\n");
+    }
+}
 
- void fechar_log(Log* log){
- 	if(log != NULL && log->arquivo != NULL){ 
- 		fclose(log->arquivo); 
- 		} 
- 	free(log); 
- 	}
+void arq_jogadaErrada(FILE* arq, int x, int y, int det){
+    if (det){
+        fprintf(arq, "Coordenadas %d, %d invalidas!\n\n" , x, y);
+    } else {
+        fprintf(arq, "Coordenadas %d, %d repetidas!\n\n" , x, y);
+    }
+}
